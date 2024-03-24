7fdc0b43697dee53a31726acaeddf53c1f7ae290
SirGustv <gust.bersan@gmail.com>
2024-03-24 14:16:20 -0300

    Primeiro exercicio
    Hello World

---
+++ URI1/Exercices/Exe1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace URI1.Exercices
+{
+    public class Exe1
+    {
+        public static void Exe()
+        {
+            Console.WriteLine("Olá, Mundo!");
+        }
+    }
+}
---
+++ URI1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using URI1.Exercices;
+
+namespace URI1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Exe1.Exe();
+    }
+}
